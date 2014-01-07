cdede175b0fdbcf73fa70795f899c8f27707371f
lee hyunyoung <setarong158@gmail.com>
2014-01-07 22:19:10 +0900

    animation  실습 프로젝트입니

---
+++ AnimationTest/ViewController.m
@@ -0,0 +1,108 @@
+//
+//  ViewController.m
+//  AnimationTest
+//
+//  Created by Lee HyunYoung on 2014. 1. 7..
+//  Copyright (c) 2014년 Lee HyunYoung. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@end
+
+@implementation ViewController {
+    BOOL moveFlag;
+}
+//배경을 흰색으로 변경, 이후에 다른 애니메이션호출
+- (IBAction)series:(id)sender {
+    [UIView beginAnimations:@"series1" context:NULL];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDuration:1.0];
+    //delegate
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(changeBackgroundClear)];
+    
+    self.imageView.backgroundColor = [UIColor blackColor];
+    
+    [UIView commitAnimations];
+    
+    //블록을 이용한 코드
+    /*
+    [UIView animateWithDuration:1.0 animations:^{
+        self.imageView.backgroundColor = [UIColor whiteColor];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:1.0 animations:^{
+            self.imageView.backgroundColor = [UIColor clearColor];
+        }];
+    }];
+    */
+}
+
+- (void)changeBackgroundClear {
+    [UIView beginAnimations:@"series2" context:NULL];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationDelegate:self];
+    
+    self.imageView.backgroundColor = [UIColor clearColor];
+    
+    [UIView commitAnimations];
+}
+
+- (IBAction)move:(id)sender {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.7];
+    
+    if (moveFlag) {
+        self.imageView.center = CGPointMake(self.imageView.center.x + 100, self.imageView.center.y + 100);
+    }else {
+        self.imageView.center = CGPointMake(self.imageView.center.x - 100, self.imageView.center.y - 100);
+    }
+    moveFlag = !moveFlag;
+    
+    [UIView commitAnimations];
+    
+    //블록을 이용한 코드
+    /*
+    [UIView animateWithDuration:0.7 animations:^{
+        if (moveFlag) {
+            self.imageView.center = CGPointMake(self.imageView.center.x + 100, self.imageView.center.y + 100);
+        }else {
+            self.imageView.center = CGPointMake(self.imageView.center.x - 100, self.imageView.center.y - 100);
+        }
+        moveFlag = !moveFlag;
+    }];
+     */
+}
+
+- (IBAction)alpha:(id)sender {
+    /*
+    NSLog(@"changing alpha");
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    
+    self.imageView.alpha = (self.imageView.alpha == 0.0) ? 1.0 : 0.0;
+    [UIView commitAnimations];
+     */
+    
+    //블록을 이용한 코드
+    [UIView animateWithDuration:0.7 animations:^{
+        self.imageView.alpha = (self.imageView.alpha == 0.0) ? 1.0 : 0.0;
+    }];
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
